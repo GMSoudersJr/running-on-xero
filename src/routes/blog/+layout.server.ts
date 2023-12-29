2ee1ec378518f036c96ce2185c609dd0769f0339
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2023-12-29 03:53:42 -0600

    remove console.log

--- src/routes/blog/+layout.server.ts
+++ src/routes/blog/+layout.server.ts
@@ -2,7 +2,6 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ parent }) => {
 	const { posts } = await parent();
-	console.log("blog Layout Server", posts);
 
 	return {
 		posts
